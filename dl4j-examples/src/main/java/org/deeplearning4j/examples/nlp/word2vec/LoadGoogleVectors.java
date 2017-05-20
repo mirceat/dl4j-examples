baf41ae49d58d2db4c95f1e9d4476d7de0450be9
Mircea Trifan <mircea.trifan@gmail.com>
2017-05-20 19:25:04 -0400

    Changed model folder to "/shared".

--- dl4j-examples/src/main/java/org/deeplearning4j/examples/nlp/word2vec/LoadGoogleVectors.java
+++ dl4j-examples/src/main/java/org/deeplearning4j/examples/nlp/word2vec/LoadGoogleVectors.java
@@ -22,7 +22,8 @@ public class LoadGoogleVectors {
 
     public static void main(String[] args) throws Exception {
         // read model
-        File gModel = new File("/similarity/models/GoogleNews-vectors-negative300.bin.gz");
+        File gModel = new File("/shared/GoogleNews-vectors-negative300.bin.gz");
+        # File gModel = new File("/similarity/models/GoogleNews-vectors-negative300.bin.gz");
         WordVectors vec = WordVectorSerializer.loadGoogleModel(gModel, true);
         
         // read words one per line     
